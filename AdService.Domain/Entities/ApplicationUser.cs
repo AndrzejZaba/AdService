a0c2c2dacfc5c0ba60624ad04130d5b409adcf7a
AndrzejZaba <andrzejzaba@onet.pl>
2024-03-10 21:12:29 +0100

    Configuration part 1

--- AdService.Domain/Entities/ApplicationUser.cs
+++ AdService.Domain/Entities/ApplicationUser.cs
@@ -3,7 +3,6 @@
 public class ApplicationUser
 {
     public string Id { get; set; }
-    public string Email { get; set; }
     public string FirstName { get; set; }
     public string LastName { get; set; }
     public DateTime RegisterDateTime { get; set; }
---
+++ AdService.Infrastructure/Persistence/Configurations/ApplicationUserConfiguration.cs
@@ -0,0 +1,34 @@
+﻿using AdService.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace AdService.Infrastructure.Persistence.Configurations;
+
+public class ApplicationUserConfiguration : IEntityTypeConfiguration<ApplicationUser>
+{
+    public void Configure(EntityTypeBuilder<ApplicationUser> builder)
+    {
+
+        builder.Property(x => x.FirstName)
+            .HasMaxLength(100);
+        
+        builder.Property(x => x.LastName)
+            .HasMaxLength(100);
+
+        builder
+            .HasOne(x => x.Address)
+            .WithOne(x => x.User)
+            .HasForeignKey<Address>(x => x.UserId)
+            .OnDelete(DeleteBehavior.Restrict);
+        
+        builder
+            .HasOne(x => x.Client)
+            .WithOne(x => x.User)
+            .HasForeignKey<Client>(x => x.UserId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+
+    }
+
+
+}
