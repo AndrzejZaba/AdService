991b7ac619f34d955211117df15129b74e72525a
AndrzejZaba <andrzejzaba@onet.pl>
2024-04-20 01:12:51 +0200

    AddCourseAdvert datepickers added

--- AdService.UI/Controllers/AdvertisementsController.cs
+++ AdService.UI/Controllers/AdvertisementsController.cs
@@ -1,10 +1,17 @@
 ﻿using AdService.Application.Advertisements.CourseAdvertisements.Commands;
+using AdService.Application.Common.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 
 namespace AdService.UI.Controllers
 {
     public class AdvertisementsController : BaseController
     {
+        private readonly IDateTimeService _dateTimeService;
+
+        public AdvertisementsController(IDateTimeService dateTimeService)
+        {
+            _dateTimeService = dateTimeService;
+        }
         public IActionResult MyJobAdvertisements()
         {
             return View();
@@ -15,7 +22,7 @@ namespace AdService.UI.Controllers
         }
         public async Task<IActionResult> AddCourseAdvertisement()
         {
-            return View(new AddCourseAdvertisementCommand());
+            return View(new AddCourseAdvertisementCommand {StartDate = _dateTimeService.Now, EndDate = _dateTimeService.Now.AddDays(7) });
         }
     }
 }
