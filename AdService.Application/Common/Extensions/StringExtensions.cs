1d2d87d0bcc6e95fb9154fe21f74febce6356e28
AndrzejZaba <andrzejzaba@onet.pl>
2024-04-28 21:54:14 +0200

    CourseAdverts cards UI fixes

--- AdService.Application/Common/Extensions/StringExtensions.cs
+++ AdService.Application/Common/Extensions/StringExtensions.cs
@@ -1,5 +1,9 @@
 ﻿
 
+using System.Net;
+using System.Text.RegularExpressions;
+using System.Xml;
+
 namespace AdService.Application.Common.Extensions;
 
 public static class StringExtensions
@@ -13,6 +17,28 @@ public static class StringExtensions
             return model;
 
         return model.Substring(0, maxLength - 3) + "...";
+    }
+
+    /// <summary>
+    /// This function only works for markups like <p> <li> <ul> and <b>
+    /// </summary>
+    /// <param name="html"></param>
+    /// <returns></returns>
+    public static string ExtractTextFromHtml(this string html)
+    {
+        if (string.IsNullOrWhiteSpace(html))
+            return string.Empty;
+
+        // Pattern to remove any HTML tags and add a space after the tag
+        string htmlTagPattern = "<.*?>";
+
+        // Replace HTML tags with a single space
+        var plainText = Regex.Replace(html, htmlTagPattern, " ");
+
+        // Optionally, reduce multiple whitespace characters to a single space
+        plainText = Regex.Replace(plainText, "\\s+", " ");
+
+        return plainText;
     }
 
 }
